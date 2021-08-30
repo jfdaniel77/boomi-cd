1e88853fbf929b89f27d17673b89a04503190c65
Daniel <jfdaniel77@gmail.com>
2021-08-30 04:44:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,187 @@
+# Boomi - Countinuous Deployment
+
+[Boomi](https://boomi.com) is an integration solution (iPaaS) to easily integrate applications, partners, and customers via the web with a visual designer and a number of pre-configured components. Boomi has a number of exciting functions and tools to create and maintain integration between two or more end-points.
+
+Boomi Integration is ran on Boomi AtomSphere Platform. Boomi AtomSphere Platform has a homepage to easily manageing processes, models, APIs, etc. It also provides programmatic access (Boomi AtomSphere API) to functionality that is normally accessed through the Boomi AtomSphere platform.
+
+This simple application is a simple solution to manage deployment processes in Boomi Integration. Using this application, we can do automate deployment and testing processes.
+
+## Pre-requisites
+- There are three environments used in this solution. Those are 
+    - DEV for development environment
+    - QAT for user acceptance testing environment
+    - PROD for production environment
+    
+- This solution deploys Processes and Flow Service components only.
+
+- Components must have particular prefix. It can follow the project name for example ```AdmissionFlowService``` as in screenshot below.
+
+- Deployment can be done in following scenarios
+    - From DEV to QAT environments
+    - From DEV to PROD environments
+    - From QAT to PROD environments
+    
+```
+Note:
+Integration Developer must have deployed the components into DEV environment manually first before using this application.
+```
+
+## Architecture
+This application is built using [Serverless Framework](https://serverless.com/) and deployed on [Amazon Web Services](https://aws.amazon.com/) environment. 
+
+This is highlevel architecture of this application
+
+This application has a REST API to receive deployment instruction and trigger the deployment process. ```Deployer Lambda``` orchestrates deployment process. It will start with payload validation. It validates following items:
+   - Source and Destination environment must be valid
+   - It contains at least one project for deployment
+
+Once validation process has been passed, it generates a token id that will be used as job id for subsequent process. Before it returns that token id, it stores token id and deployment instruction to database and put token id to queue system.
+
+```
+Note:
+We use Amazon DynamoDB and Amazon SQS for storage and queue system respectivelly.
+```
+
+This event will trigger ```Worker Lambda``` to pick up job id. Using this job id, this ```lambda``` will get deployment instruction from database and start to interact with ```AtomSphere API```. These are steps done by this ```lambda```:
+- Get all components based on project prefix
+- Get components' package id from source environment
+- Deploy components to destination environment
+
+At the end of process, this ```lambda``` will update job status and put the deployment result in database.
+
+This application has another REST API to get  deployment status. It can be pooled till it returns final result or status. The possible status that it returns are:
+- ```IN PROGRESS```
+Process is still in progress.
+- ```DONE```
+The deployment process is done.
+
+## APIs
+These are REST APIs for this application.
+
+|Service|Method|API|Response Status|
+|-------|-------|-------|-------|
+|Deploy components|POST|/release| ```200``` Success<br>```400``` Invalid input parameter|
+|Get result status|GET|/release/{token_id}| ```200``` Success<br>```400``` Token error|
+
+
+```
+Note: For authententication, both APIs require an API Key to be put in header.
+```
+
+## Usage
+We can use any tools that can send HTTP Request such as ```curl```, [Postman](https://www.postman.com/) , [Insomnia](https://insomnia.rest/).
+
+### Deploy components
+This is payload that is required for component deployment.
+
+```json
+{
+    "environment": {
+        "source": "DEV",
+        "destination": "QAT"
+    },
+    "projects": [
+        {
+            "projectName": "Admissions",
+            "componentPrefix": "AdmissionFlowService"
+        },
+        {
+            "projectName": "Admissions - Scholarship",
+            "componentPrefix": "AdmissionFlowService"
+        }
+    ],
+    "notes": "Deployment"
+}
+```
+
+| Element | Description |
+|-------|-------|
+| [environment].[source] | Source environment. <br>Possible values are either DEV or QAT. |
+| [environment].[target] | Destination environment. <br>Possible values are either QAT or PROD. |
+| [projects] | List of project that will be deployed from Source environment to Destination environment. |
+| [projects].[projectName]| Project name.<br>It represents folder name in Boomi Integration Platform. |
+| [projects].[componentPrefix]| Prefix for components that are deployed to Destination environment. |
+| [notes] | Notes that will be put in deployment description |
+
+This is response after the job has been passed successfully to queue system.
+
+```json
+{
+    "token": "bf335869-be41-4d2a-a028-94c441ac0678"
+}
+```
+| Element | Description |
+|-------|-------|
+| [token] | Unique token id that will be used in subsequent request to get result status |
+
+### Get result status
+To get result status, we need to pass token from previous request and put it in query path.
+
+This is a response when deployment process is still in-progress.
+
+```json
+{
+    "token": "bf335869-be41-4d2a-a028-94c441ac0678",
+    "input": {
+        "environment": {
+            "source": "DEV",
+            "destination": "QAT"
+        },
+        "projects": [
+            {
+                "projectName": "Admissions",
+                "componentPrefix": "AdmissionFlowService"
+            },
+            {
+                "projectName": "Admissions - Scholarship",
+                "componentPrefix": "AdmissionFlowService"
+            }
+        ],
+        "notes": "Deployment",
+        "token": "bf335869-be41-4d2a-a028-94c441ac0678"
+    },
+    "status": "IN PROGRESS"
+}
+```
+
+This is response after deployment process has been done.
+
+```json
+{
+    "token": "bf335869-be41-4d2a-a028-94c441ac0678",
+    "input": {
+        "environment": {
+            "source": "DEV",
+            "destination": "QAT"
+        },
+        "projects": [
+            {
+                "projectName": "Admissions",
+                "componentPrefix": "AdmissionFlowService"
+            },
+            {
+                "projectName": "Admissions - Scholarship",
+                "componentPrefix": "AdmissionFlowService"
+            }
+        ],
+        "notes": "Deployment",
+        "token": "bf335869-be41-4d2a-a028-94c441ac0678"
+    },
+    "status": "DONE",
+    "component": [...]
+}
+```
+
+| Element | Description |
+|-------|-------|
+| [token] | Unique token id that identify a job |
+| [input] | Requuest payload for this job |
+| [status] | Deployment Status (```DONE``` or ```IN PROGRESS```) |
+| [component] | List of components |
+
+
+## Future Enhancements
+- In this first release, the application will deploy all-in-one.
+We can enhance it to have capabilities to deploy particular component.
+
+- It will be good to provide interactive front end so that user can monitor and get real time status for individual component being deployed.
---
+++ handler.py
@@ -0,0 +1,446 @@
+import os
+import requests
+import time
+import boto3
+import uuid
+
+from json import loads, dumps
+from requests.auth import HTTPBasicAuth
+from json import dumps, loads
+from datetime import datetime
+    
+# Globar parameters
+headers = {"Content-type": "application/json", "Accept": "application/json"}
+
+# Account credentials
+boomi_account_id = os.environ["BOOMI_ACCOUNT_ID"]
+boomi_username = os.environ["BOOMI_USERNAME"]
+boomi_api_token = os.environ["BOOMI_API_TOKEN"]
+
+# Boomi environment id
+boomi_environments = {
+    "DEV": {
+        "environmentId": os.environ["DEV_ENV_ID"]
+    },
+    "QAT": {
+        "environmentId": os.environ["QAT_ENV_ID"]
+    },
+    "PROD": {
+        "environmentId": os.environ["PRD_ENV_ID"]
+    }
+}
+
+# Boomi AtomSphere API Endpoints
+boomi_atom_sphere_api_endpoint = os.environ["BOOMI_API_ENDPOINT"]
+boomi_component_meta_endpoint = "{}/{}/{}".format(boomi_atom_sphere_api_endpoint, boomi_account_id, "ComponentMetadata/query")
+boomi_component_meta_more_endpoint = "{}/{}/{}".format(boomi_atom_sphere_api_endpoint, boomi_account_id, "ComponentMetadata/queryMore")
+boomi_environment_endpoint = "{}/{}/{}".format(boomi_atom_sphere_api_endpoint, boomi_account_id, "Environment/query")
+boomi_deploy_package_query_endpoint = "{}/{}/{}".format(boomi_atom_sphere_api_endpoint, boomi_account_id, "DeployedPackage/query")
+boomi_deploy_package_endpoint = "{}/{}/{}".format(boomi_atom_sphere_api_endpoint, boomi_account_id, "DeployedPackage/")
+
+class Error(Exception):
+    """Base-class for all exceptions raised by this module"""
+    
+class ValueError(Error):
+    """Base-class for value errors"""
+    
+class ConnectionError(Error):
+    """Base-class for connection to Boomi AtomSphere API errors, especially for those not 2xx HTTP Response Status"""
+    
+def get_result(event, context):
+    token = ""
+    
+    # Get input parameter
+    if event.get("pathParameters") and event.get("pathParameters").get("token"):
+        token = event.get("pathParameters").get("token")
+    else:
+        return {"statusCode": 400, "body": "Missing Token", "headers": {"Content-Type": "text/plain"}}
+        
+    # Get result from DynamoDB
+    client = boto3.client('dynamodb')    
+    response = client.get_item(
+                        TableName="boomi-cd-table",
+                        Key={
+                            "token": {
+                                'S': token
+                                }
+                            })
+    
+    if response.get("Item"):
+        
+        if response.get("Item").get("process_status").get("S") == "IN PROGRESS":
+            data = {
+                "token": token,
+                "input": loads(response.get("Item").get("input").get("S")),
+                "status": response.get("Item").get("process_status").get("S")
+            }
+        else:
+            data = {
+                "token": token,
+                "input": loads(response.get("Item").get("input").get("S")),
+                "status": response.get("Item").get("process_status").get("S"),
+                "component": loads(response.get("Item").get("component").get("S"))
+            }
+    else: 
+        return {"statusCode": 400, "body": {"message": "Incorrect Token"}}
+                            
+    return {"statusCode": 200, "body": dumps(data, indent=2)}
+    
+def deploy(event, context):
+    
+    # Get input parameter
+    input_parameter = loads(event.get("body"))
+    
+    # Validate input parameter and environment
+    try:
+        validate_input_parameter(input_parameter)
+    except ValueError as ve:
+        return {"statusCode": 400, "body": str(ve), "headers": {"Content-Type": "text/plain"}}
+    except ConnectionError as ce:
+        return {"statusCode": 500, "body": str(ce), "headers": {"Content-Type": "text/plain"}}
+    except Exception as e:
+        return {"statusCode": 500, "body": "Process Error", "headers": {"Content-Type": "text/plain"}}
+    
+    # Generate token and set into input_parameter for next process
+    token = str(uuid.uuid4())
+    input_parameter['token'] = token
+    
+    # Create a new record in DynamoDB
+    client = boto3.client("dynamodb")
+    response = client.put_item(
+                        TableName="boomi-cd-table",
+                        Item={
+                            "token": {
+                                "S": token
+                            },
+                            "process_status": {
+                                "S": "IN PROGRESS"
+                            },
+                            "input": {
+                                "S": dumps(input_parameter)
+                            }
+                        })
+                        
+    # Push input parameter to SQS
+    client = boto3.client('sqs')
+    response = client.send_message(QueueUrl=os.environ["SQS_QUEUE_URL"], MessageBody=dumps(input_parameter))
+    
+    # return token
+    return {"statusCode": 200, "body": dumps({"token": token})}
+    
+    
+def process_deployment(event, context):
+    
+    # Get input parameter
+    input_parameter = loads(event.get("Records")[0].get("body"))
+    
+    print("Process deployment : {}".format(event))
+        
+    # Set required values to local variables
+    token = input_parameter.get("token")
+    env_source = input_parameter.get('environment').get('source')
+    env_destination = input_parameter.get('environment').get('destination')
+    projects = input_parameter.get('projects')
+    notes = input_parameter.get('notes')
+    
+    # Populate component detail
+    list_component = populate_component(projects)
+    
+    # Get Package ID for all components
+    populate_packageId(env_source, list_component)
+    
+    # Deploy package to destination environment
+    deploy_package(env_destination, list_component, notes)
+    
+    # Create a new record in DynamoDB
+    client = boto3.client("dynamodb")
+    response = client.update_item(
+                                TableName="boomi-cd-table",
+                                Key={
+                                    "token": {
+                                        "S": token
+                                        
+                                    }
+                                },
+                                UpdateExpression="set component = :val1, process_status = :val2",
+                                ExpressionAttributeValues={
+                                    ":val1": {
+                                        "S": dumps(list_component)
+                                        
+                                    },
+                                    ":val2": {
+                                        "S": "DONE"
+                                    }
+                                },
+                                ReturnValues="UPDATED_NEW"
+                            )
+        
+    return {"statusCode": 200, "body": "Success", "Content-Type":"text/plain"}
+    
+def deploy_package(env_destination, list_component, notes):
+    
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    notes = "{} - {} ".format(dt_string, notes)
+    
+    for item in list_component:
+        
+        id = item.get("packageId")
+        name = item.get("name")
+        
+        payload = { "environmentId" : boomi_environments[env_destination]['environmentId'],
+                    "packageId" : id,
+                    "notes" : notes
+                }
+    
+        response = requests.post(boomi_deploy_package_endpoint, 
+                             headers=headers, 
+                             auth=HTTPBasicAuth(boomi_username, boomi_api_token), 
+                             data=dumps(payload))
+        
+        if response.status_code == 200:
+            item["status"] = "DEPLOYED"
+            
+        elif response.status_code == 400:
+            item["status"] = "EXISTING"
+            
+        else:
+            item["status"] = "ERROR"
+            item["errorMessage"] = response.text
+            
+            
+        time.sleep(1)
+            
+    return list_component
+    
+def populate_packageId(env_source, list_component):
+    
+    for item in list_component:
+        
+        id = item.get("componentId")
+        name = item.get("name")
+        
+        payload = {
+            "QueryFilter": {
+                "expression": {
+                    "operator": "and",
+                    "nestedExpression": [
+                        {
+                            "argument": [
+                                id
+                            ],
+                            "operator": "EQUALS",
+                            "property": "componentId"
+                        },
+                        {
+                            "argument": [
+                                boomi_environments[env_source]['environmentId']
+                            ],
+                            "operator": "EQUALS",
+                            "property": "environmentId"
+                        },
+                        {
+                            "argument": [
+                                True
+                            ],
+                            "operator": "EQUALS",
+                            "property": "active"
+                        }
+                    ]
+                }
+            }
+        }
+        
+        response = requests.post(boomi_deploy_package_query_endpoint, 
+                             headers=headers, 
+                             auth=HTTPBasicAuth(boomi_username, boomi_api_token), 
+                             data=dumps(payload))
+        
+        if response and response.text:
+            resp_body = loads(response.text)
+        
+            if resp_body and resp_body.get('result') and len(resp_body.get('result')) > 0:
+                packageId = resp_body.get('result')[0]['packageId']
+                item["packageId"] = packageId
+                print("{} :: componentId: {} :: packageId: {}".format(name, id, packageId))
+            else:
+                print("{} :: componentId: {} :: packageId: N/A".format(name, id))
+        else:
+            print("{} :: Invalid Response: {} - {}".format(name, response, response.text))
+            
+        time.sleep(1)
+            
+    
+def populate_component(projects):
+    list_component = []
+
+    for item in projects:
+        payload = {
+            "QueryFilter": {
+                "expression": {
+                    "operator": "and",
+                    "nestedExpression": [
+                        {
+                            "argument": [
+                                "{}%".format(item.get("componentPrefix"))
+                            ],
+                            "operator": "LIKE",
+                            "property": "name"
+                        },
+                        {
+                            "argument": [
+                                item.get("projectName")
+                            ],
+                            "operator": "EQUALS",
+                            "property": "folderName"
+                        },
+                        {
+                            "argument": [
+                                False
+                            ],
+                            "operator": "EQUALS",
+                            "property": "deleted"
+                        },
+                        {
+                            "argument": [
+                                True
+                            ],
+                            "operator": "EQUALS",
+                            "property": "currentVersion"
+                        }
+                    ]
+                }
+            }
+        }
+        
+        response = requests.post(boomi_component_meta_endpoint, 
+                             headers=headers, 
+                             auth=HTTPBasicAuth(boomi_username, boomi_api_token), 
+                             data=dumps(payload))
+    
+        resp_body = loads(response.text)
+        if resp_body.get('queryToken'):
+            query_token = resp_body['queryToken'] 
+        else:
+            query_token = None
+                
+        print("Number of total records in result : {}".format(resp_body['numberOfResults']))
+        list_component.extend(resp_body['result'])
+    
+        has_next = False
+    
+        if query_token:
+            has_next = True
+    
+        while (has_next):
+            payload = query_token
+            response = requests.post(boomi_component_meta_more_endpoint, 
+                                 headers=headers, 
+                                 auth=HTTPBasicAuth(boomi_username, boomi_api_token), 
+                                 data=query_token)
+            resp_body = loads(response.text)
+    
+            if resp_body.get('queryToken'):
+                query_token = resp_body['queryToken'] 
+            else:
+                query_token = None
+    
+            list_component.extend(resp_body['result'])
+    
+            if query_token:
+                continue
+            else:
+                has_next = False
+                
+    print("Number of total records in list : {}".format(len(list_component)))
+    return list_component
+    
+def verify_environment(environment_id):
+    is_valid = False
+    
+    payload = {
+      "QueryFilter" :
+        {
+          "expression" :
+            {
+              "argument" : [environment_id],
+              "operator" : "EQUALS",
+              "property" : "id"
+            }
+        }
+    }
+
+    response = requests.post(boomi_environment_endpoint, 
+                             headers=headers, 
+                             auth=HTTPBasicAuth(boomi_username, boomi_api_token), 
+                             data=dumps(payload))
+
+    if response.status_code == 200:
+        response_body = loads(response.text)
+        
+        if response_body.get("result"):
+            if response_body.get("result")[0]['id']:
+                is_valid = True
+    else:
+        raise ConnectionError("Response from Boomi AtomSphereAPI : {}\nError message: {}".format(response.status_code, response.text))
+
+    
+def validate_input_parameter(input_parameter):
+    # Check if input_parameter is available
+    if input_parameter is None:
+        raise ValueError("Error: Missing input parameter")
+        
+    # Check if environment (source and destination) is available
+    if input_parameter.get("environment") is None:
+        raise ValueError("Error: Environment is mandatory")
+    else:
+        environment = input_parameter.get("environment")
+        
+        if environment.get("source") is None:
+            raise ValueError("Error: Source Environment is mandatory")
+        
+        if environment.get("destination") is None:
+            raise ValueError("Error: Destination Environment is mandatory")
+            
+    # Check if projects is available
+    if input_parameter.get("projects") is None:
+        raise ValueError("Error: Projects is mandatory")
+    else:
+        projects = input_parameter.get("projects")
+        
+        if len(projects) == 0:
+            raise ValueError("Error: Projects must have at least one project")
+            
+    # Check value of source and destination environments
+    available_env = list(boomi_environments.keys())
+    available_source = ["DEV", "QAT"]
+    available_destination = ["QAT", "PROD"]
+    
+    if input_parameter.get("environment").get('source') not in available_env:    
+        raise ValueError("Error: Source environment is not available")
+        
+    if input_parameter.get("environment").get('destination') not in available_env:    
+        raise ValueError("Error: Destination environment is not available")
+        
+    if input_parameter.get("environment").get('source') == input_parameter.get("environment").get('destination'):
+        raise ValueError("Error: Source and Destination environment cannot be same value")
+        
+    if input_parameter.get("environment").get('source') not in available_source:    
+        raise ValueError("Error: Source environment is only available for DEV and QAT")
+        
+    if input_parameter.get("environment").get('destination') not in available_destination:    
+        raise ValueError("Error: Destination environment is only available for QAT and PROD")
+    
+    # Check project name and component prefix
+    for project in projects:
+        if project.get('projectName') is None:
+            raise ValueError("Error: Project Name is mandatory")
+            
+        if project.get('componentPrefix') is None:
+            raise ValueError("Error: Component Prefix is mandatory")
+
+    env_source = input_parameter.get('environment').get('source')
+    verify_environment(boomi_environments[env_source]["environmentId"])
+    
+    env_destination = input_parameter.get('environment').get('destination')
+    verify_environment(boomi_environments[env_destination]["environmentId"])
